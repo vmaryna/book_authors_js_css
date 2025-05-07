9f82f40b6920958cc919b7f6c87af0897afbcd3c
Maryna <maryna.vertay@powercoders.org>
2025-05-07 09:56:50 +0200

    downloaded initial project files

---
+++ scripts.js
@@ -0,0 +1,50 @@
+let authors = [
+    {
+        firstName: 'Andreas',
+        lastName: 'Neeser'
+    },
+    {
+        firstName: 'Anna',
+        lastName: 'Ruchat'
+    },
+    {
+        firstName: 'Arno',
+        lastName: 'Camenisch'
+    },
+    {
+        firstName: 'Barbara',
+        lastName: 'Schibli'
+    },
+    {
+        firstName: 'Demian',
+        lastName: 'Leinhard'
+    },
+    {
+        firstName: 'Flurina',
+        lastName: 'Bader'
+    },
+    {
+        firstName: 'Franco',
+        lastName: 'Supino'
+    },
+    {
+        firstName: 'Lukas',
+        lastName: 'Hartmann'
+    },
+    {
+        firstName: 'Marius',
+        lastName: 'Popescu'
+    },
+    {
+        firstName: 'Reto',
+        lastName: 'Haenny'
+    },
+    {
+        firstName: 'Sandra',
+        lastName: 'Kuenzi'
+    },
+    {
+        firstName: 'Simon',
+        lastName: 'Libsig'
+    }
+];
